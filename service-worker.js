7414d1647e746bd39c37ee81280f58d50f3b2b1d
Salvador <salvacams@gmail.com>
2020-02-06 15:11:33 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'polen-v0.1.03';
+var cacheName = 'polen-v0.1.04';
 
 var filesToCache = [
   './',
@@ -36,8 +36,8 @@ self.addEventListener('activate', function(e) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request);
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
