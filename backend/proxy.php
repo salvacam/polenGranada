a75421efbd76a2066e2d62bbf0286899dc07f3f2
salvacam <salvacam@gmail.com>
2020-05-09 18:18:23 +0200

    change backend for image

--- backend/proxy.php
+++ backend/proxy.php
@@ -1,13 +1,16 @@
 <?php
 
+/*
 header('Content-Type: text/plain; charset=utf-8');
 header("access-control-allow-origin: *");
+*/
 
 require './src/JsonDB.class.php';
 
 date_default_timezone_set('Europe/Madrid');
 
 $db = new JsonDB("./db/");
+$img = './img/data.png';	
 
 /*
 ini_set('display_errors', 1);
@@ -20,47 +23,56 @@ $now = new DateTime();
 $fechaHoraActual = $now->getTimestamp();
 
 $datosGuardados = $db->selectAll("data");
+$fechaHoraLimite = 0;
+
 if (count($datosGuardados) > 0) {
 		
-	$fechaHoraLimite = $datosGuardados[0]["DateTime"] + 18000; //Five hours, 60 seg * 60 min * 5 hour
-	if ($fechaHoraLimite > $fechaHoraActual) {
-		// Devolver lo guardado
-		http_response_code(200);
-		echo html_entity_decode($datosGuardados[0]["SRC"]);
-		die();
-	}
+	$fechaHoraLimite = $datosGuardados[0]["DateTime"] + 18000; //Five hours, 60 seg * 10 min * 5 hour
 }
 
-//$urlFacebook = "https://www.facebook.com/Informaci%C3%B3n-Polen-Granada-128331124035311/";
-$urMeteoblue = "https://www.meteoblue.com/es/tiempo/outdoorsports/airquality/granada_españa_2517117";
+if ($fechaHoraLimite > $fechaHoraActual) {
+	// Devolver lo guardado
+	http_response_code(200);
+	echo html_entity_decode($datosGuardados[0]["SRC"]);
+	die();
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $urMeteoblue);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch,CURLOPT_FOLLOWLOCATION,true);
-curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.3) Gecko/20070309 Firefox/2.0.0.3");
-curl_setopt($ch, CURLOPT_REFERER, "https://www.meteoblue.com");
-$page = curl_exec($ch);
-curl_close($ch);
+} else  {
 
-$textSearch = "data-original='";
-$longTextSearch = strlen($textSearch);
+	//$urlFacebook = "https://www.facebook.com/Informaci%C3%B3n-Polen-Granada-128331124035311/";
+	$urMeteoblue = "https://www.meteoblue.com/es/tiempo/outdoorsports/airquality/granada_españa_2517117";
 
-$endPost = 0;
-$starPosition = strpos($page, '<div id="blooimage"', $endPost);
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $urMeteoblue);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+	curl_setopt($ch,CURLOPT_FOLLOWLOCATION,true);
+	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.3) Gecko/20070309 Firefox/2.0.0.3");
+	curl_setopt($ch, CURLOPT_REFERER, "https://www.meteoblue.com");
+	$page = curl_exec($ch);
+	curl_close($ch);
 
-$initPosition = strpos($page, $textSearch, $starPosition);
+	$textSearch = "data-original='";
+	$longTextSearch = strlen($textSearch);
 
-$endPost = strpos($page, "'", $initPosition + $longTextSearch);
+	$endPost = 0;
+	$starPosition = strpos($page, '<div id="blooimage"', $endPost);
 
-$result = substr($page, $initPosition + $longTextSearch, $endPost - ($initPosition + $longTextSearch));
+	$initPosition = strpos($page, $textSearch, $starPosition);
 
-// Guardar fecha y hora cuando se realiza la llamada
-$lista = array('ID'=>0, 'SRC'=> $result, 'DateTime'=>$fechaHoraActual);
+	$endPost = strpos($page, "'", $initPosition + $longTextSearch);
 
-// Guardar resultado procesado de la llamada
-$db -> deleteAll ( "data" );
-$db->insert("data", $lista, true);
+	$result = substr($page, $initPosition + $longTextSearch, $endPost - ($initPosition + $longTextSearch));
 
-http_response_code(200);
-echo html_entity_decode($result);
+	// Guardar fecha y hora cuando se realiza la llamada
+	$lista = array('ID'=>0, 'SRC'=> $result, 'DateTime'=>$fechaHoraActual);
+
+	// Guardar resultado procesado de la llamada
+	$db -> deleteAll ( "data" );
+	$db->insert("data", $lista, true);
+
+	//Crear imagen
+	file_put_contents($img, file_get_contents("https:" . html_entity_decode($result)));
+	
+	http_response_code(200);
+	echo html_entity_decode($result);/
+
+}
