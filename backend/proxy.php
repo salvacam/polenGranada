4c98d1e0a13a4efad971955a0bb5e8b01c90ad6d
salvacam <salvacams@gmail.com>
2025-06-03 22:59:21 +0200

    update URL

--- backend/proxy.php
+++ backend/proxy.php
@@ -11,11 +11,11 @@ date_default_timezone_set('Europe/Madrid');
 $db = new JsonDB("./db/");
 $img = './img/data.png';	
 
-/*
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-*/
+
 
 // Comprobar si la ciudad esta cacheada en menos de 5horas
 $now = new DateTime();
@@ -24,23 +24,24 @@ $fechaHoraActual = $now->getTimestamp();
 $datosGuardados = $db->selectAll("data");
 $fechaHoraLimite = 0;
 
-if (count($datosGuardados) > 0) {
-		
-	$fechaHoraLimite = $datosGuardados[0]["DateTime"] + 18000; //Five hours, 60 seg * 60 min * 5 hour
+if (count($datosGuardados) > 0) {		
+	$fechaHoraLimite = $datosGuardados[0]["DateTime"] + 28800; //Eight hours, 60 seg * 60 min * 8 hour
+	//$fechaHoraLimite = $datosGuardados[0]["DateTime"] + 18000; //Five hours, 60 seg * 60 min * 5 hour
+	//$fechaHoraLimite = $datosGuardados[0]["DateTime"] + 10800; //Three hours, 60 seg * 60 min * 3 hour
 }
 
 if ($fechaHoraLimite > $fechaHoraActual) {
 	// Devolver lo guardado
 	http_response_code(200);
-	echo html_entity_decode($datosGuardados[0]["SRC"]);
+	//echo html_entity_decode($datosGuardados[0]["SRC"]);
+	echo html_entity_decode('backend/img/data.png');
 	die();
 
 } else  {
 
 	//$urlFacebook = "https://www.facebook.com/Informaci%C3%B3n-Polen-Granada-128331124035311/";
-	$urMeteoblue = "https://www.meteoblue.com/es/tiempo/outdoorsports/airquality/granada_españa_2517117";
-	//https://my.meteoblue.com/visimage/meteogram_airquality?look=KILOMETER_PER_HOUR%2CCELSIUS%2CMILLIMETER&temperature=C&windspeed=kmh&precipitationamount=mm&winddirection=3char&city=Granada&iso2=es&lat=37.1882&lon=-3.60667&asl=689&tz=Europe%2FMadrid&dpi=72&apikey=5838a18e295d&lang=es&ts=1743457537&sig=9cbc2bc543af783f4fd89066a005a43e
-
+	$urMeteoblue = "https://www.meteoblue.com/es/tiempo/outdoorsports/airquality/granada_espa%c3%b1a_2517117";
+/*
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $urMeteoblue);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
@@ -49,7 +50,10 @@ if ($fechaHoraLimite > $fechaHoraActual) {
 	curl_setopt($ch, CURLOPT_REFERER, "https://www.meteoblue.com");
 	$page = curl_exec($ch);
 	curl_close($ch);
+*/
+	$page = file_get_contents($urMeteoblue);
 
+	//echo var_dump($page) . "<br>";
 	$textSearch = "data-original='";
 	$longTextSearch = strlen($textSearch);
 
@@ -61,7 +65,7 @@ if ($fechaHoraLimite > $fechaHoraActual) {
 	$endPost = strpos($page, "'", $initPosition + $longTextSearch);
 
 	$result = substr($page, $initPosition + $longTextSearch, $endPost - ($initPosition + $longTextSearch));
-
+	
 	// Guardar fecha y hora cuando se realiza la llamada
 	$lista = array('ID'=>0, 'SRC'=> $result, 'DateTime'=>$fechaHoraActual);
 
@@ -73,6 +77,6 @@ if ($fechaHoraLimite > $fechaHoraActual) {
 	file_put_contents($img, file_get_contents("https:" . html_entity_decode($result)));
 	
 	http_response_code(200);
-	echo html_entity_decode($result);
-
 }
+
+echo html_entity_decode('https://salvacam.x10.mx/polen/backend/img/data.png');
